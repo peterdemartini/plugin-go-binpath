079f6954d559a33872c30028b58d6f125eaad928
Peter DeMartini <thepeterdemartini@gmail.com>
2017-10-31 09:10:19 -0700

    should be GOPATH/bin

--- init.fish
+++ init.fish
@@ -1,5 +1,5 @@
 function init -a path --on-event init_go-binpath
-  set -l go_bin_path $GOPATH 
+  set -l go_bin_path $GOPATH/bin 
   contains -- $go_bin_path $PATH
     or set -gx PATH $go_bin_path $PATH
 end
